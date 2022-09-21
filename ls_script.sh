aab2590a781d711ee6607498f7a51c6c12429a1f
jaywor1 <103755136+jaywor1@users.noreply.github.com>
2022-09-21 08:19:58 +0000

    changed the name of output.txt to old_names.txt + simplified

--- ls_script.sh
+++ ls_script.sh
@@ -2,24 +2,18 @@
 
 WDIR=$(pwd)
 
-OUTPUT=$WDIR/output.txt
+OUTPUT=$WDIR/old_names.txt
 
 if [ -f "$OUTPUT" ]; then
 	echo "$OUTPUT exists do you want to overwrite the file? [y,n]"
 	read DECISION
 	if [ $DECISION == "y" ] || [ $DECISION == "Y" ]; then
-		ls > output.txt
+		ls > $OUTPUT
 	elif [ $DECISION == "n" ] || [ $DECISION == "N" ]; then
-		echo "output.txt wasn't deleted"
+		echo "old_names.txt wasn't deleted"
 	else
 		echo "wrong input no changes were made"
 	fi	
 else
-	ls > output.txt
+	ls > $OUTPUT
 fi
-
-ARR=( $(ls) )
-
-for str in ${ARR[@]}; do
-	echo $str
-done
